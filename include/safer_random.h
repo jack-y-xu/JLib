3ced61d81af807f52747177ad3956dde1f1d587e
Jack <31490726+jack-y-xu@users.noreply.github.com>
2023-07-25 00:30:20 -0400

    Create safer_random.h

---
+++ include/safer_random.h
@@ -0,0 +1,91 @@
+
+#ifndef SAFER_RANDOM
+#define SAFER_RANDOM
+
+#include <concepts>
+#include <type_traits>
+#include <ranges>
+#include <numeric>
+#include <random>
+#include <stdexcept>
+
+namespace SaferRandom {
+
+    using namespace std::ranges;
+
+    template <typename T>
+    concept true_floating = !std::integral<T> && std::floating_point<T>;
+
+    namespace {
+        struct GeneratorInitWrapper {
+            unsigned int seed;
+            std::mt19937 generator;
+            GeneratorInitWrapper() {
+                auto rd = std::random_device();
+                seed = rd();
+                generator = std::mt19937(seed);
+            };
+            GeneratorInitWrapper(unsigned int s): seed(s), generator(s) {};
+        };
+    }
+
+    class ParallelRandom {
+    public:
+        inline static std::mt19937& getGenerator() {
+            static thread_local GeneratorInitWrapper gen;
+            return gen.generator;
+        }
+
+        template <true_floating T>
+        inline static T random() {
+            static std::uniform_real_distribution<T> dist(0, 1);
+            return dist(getGenerator());
+        }
+
+        template<true_floating T>
+        inline static T random(T low, T high) {
+            return (high - low) * random<T>() + low;
+        }
+
+        inline static int randint(int low, int high_inclusive) {
+            std::uniform_int_distribution<> dist(low, high_inclusive);
+            return dist(getGenerator());
+        }
+
+        template <true_floating T>
+        inline static bool bernoulli(T p) {
+            return random<T>() <= p;
+        }
+
+        template <range P, range E, typename T>
+        requires true_floating<range_value_t<P>> && std::same_as<range_value_t<E>, T>
+        static T weighted_select(const P& p, const E& e, T fallback, bool safety_check=true) {
+            if (safety_check) {
+                // size is O(n) if not using sized_range
+                if (size(p) != size(e))
+                    throw std::runtime_error("Range sizes must be the same");
+
+                if (std::any_of(p.begin(), p.end(), [](auto& x) {return x < 0 || x > 1;}))
+                    throw std::runtime_error("All probabilities must be between 0 and 1");
+
+                double sum = std::accumulate(p.begin(), p.end(), 0.0);
+                if (sum > 1 || sum < 0)
+                    throw std::runtime_error("Probabilities must sum up to between 0 and 1");
+            }
+            double rand = random<double>(), partial = 0;
+            auto pi = p.begin(), ei = e.begin();
+
+            while (pi != p.end()) {
+                partial += *pi;
+                if (rand <= partial)
+                    return *ei;
+                pi++;
+                ei++;
+            }
+
+            return fallback;
+        }
+    };
+}
+
+#endif
